2b4696940c3439885a047833dbf7093770367a57
kashmii <www.myac@gmail.com>
2023-07-23 18:07:55 +0900

    オウム返し実装

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/kashmii/isshu100_linebot_go
+
+go 1.20
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/line/line-bot-sdk-go v7.8.0+incompatible
+)
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	// ハンドラの登録
+	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/callback", lineHandler)
+
+	fmt.Println("http://localhost:8080 で起動中...")
+	// HTTPサーバを起動
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	msg := "Hello World!!!!"
+	fmt.Fprintf(w, msg)
+}
+
+func lineHandler(w http.ResponseWriter, r *http.Request) {
+	// BOTを初期化
+	bot, err := linebot.New(
+		os.Getenv("LINE_CHANNEL_SECRET"),
+		os.Getenv("LINE_CHANNEL_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// リクエストからBOTのイベントを取得
+	events, err := bot.ParseRequest(r)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
+	}
+	for _, event := range events {
+		// イベントがメッセージの受信だった場合
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			// メッセージがテキスト形式の場合
+			case *linebot.TextMessage:
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+					log.Print(err)
+				}
+			}
+		}
+	}
+}
