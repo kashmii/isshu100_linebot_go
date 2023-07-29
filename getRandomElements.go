74a0e2e75f78e86a26c08b0ce894294cdb56d473
kashmii <www.myac@gmail.com>
2023-07-29 19:58:28 +0900

    ランダムに設問と選択肢を取得して表示する機能実装

---
+++ getRandomElements.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+func GetRandomWakas(slice []Waka, numElements int) []Waka {
+	if numElements <= 0 || numElements > len(slice) {
+		// 要素数が0以下または元のスライスよりも大きい場合は空のスライスを返す
+		return []Waka{}
+	}
+
+	rand.Seed(time.Now().UnixNano()) // ランダムシードを設定
+	result := make([]Waka, numElements)
+
+	// ランダムなインデックスを生成して、対応する要素を新しいスライスに追加
+	for i := 0; i < numElements; i++ {
+		randomIndex := rand.Intn(len(slice))
+		result[i] = slice[randomIndex]
+
+		// 重複しないように選択済みの要素をスライスから削除
+		// 削除することでスライスが詰められるので、ランダムな選択が保証されます
+		slice = append(slice[:randomIndex], slice[randomIndex+1:]...)
+	}
+
+	return result
+}
--- main.go
+++ main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"reflect"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/line/line-bot-sdk-go/linebot"
 )
+
+type Waka struct {
+	No      int    `json:"no"`
+	Kami    string `json:"kami"`
+	Simo    string `json:"simo"`
+	Kami_kana    string `json:"kami_kana"`
+	Simo_kana    string `json:"simo_kana"`
+	Author    string `json:"sakusya"`
+	Author_kana    string `json:"sakusya_name"`
+	Image    string `json:"image"`
+}
 
 func main() {
 	err := godotenv.Load(".env")
@@ -40,6 +56,52 @@ func lineHandler(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
+	// ===============
+	// JSONファイルを読み込む
+	file, err := os.Open("master.json")
+	if err != nil {
+		fmt.Println("JSONファイルをオープンできませんでした:", err)
+		return
+	}
+	defer file.Close()
+
+	// ファイル内容をバイト配列に読み込む
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println("JSONファイルの読み込みに失敗しました:", err)
+		return
+	}
+
+	// バイト配列を構造体に変換
+	var masterData []Waka
+	err = json.Unmarshal(data, &masterData)
+	if err != nil {
+		fmt.Println("JSONデータの解析に失敗しました:", err)
+		return
+	}
+	t := reflect.TypeOf(masterData)
+	fmt.Println("データ型:", t)
+
+	// 和歌を4つ取得して配列：長さ3に入れる
+	// 配列[0]は正解として、それ以外ははずれ選択肢として使う
+	randomElements := GetRandomWakas(masterData, 4)
+	choices := make([]string, 4)
+
+	for i, ele := range randomElements {
+		choices[i] = ele.Simo
+	}
+
+	rand.Seed(time.Now().UnixNano()) //乱数のシード設定
+
+	rand.Shuffle(len(choices), func(i, j int) {
+		choices[i], choices[j] = choices[j], choices[i]
+	})
+
+	// ===============
+	// ===============
+
+
+
 	// リクエストからBOTのイベントを取得
 	events, err := bot.ParseRequest(r)
 	if err != nil {
@@ -50,16 +112,77 @@ func lineHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
+
+
 	for _, event := range events {
 		// イベントがメッセージの受信だった場合
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
 			// メッセージがテキスト形式の場合
 			case *linebot.TextMessage:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
-					log.Print(err)
-				}
+				switch message.Text {
+				case "flex":
+						resp := linebot.NewFlexMessage(
+								"this is a flex message",
+								&linebot.BubbleContainer{
+										Type: linebot.FlexContainerTypeBubble,
+										Body: &linebot.BoxComponent{
+												Type:   linebot.FlexComponentTypeBox,
+												Layout: linebot.FlexBoxLayoutTypeVertical,
+												Contents: []linebot.FlexComponent{
+														&linebot.TextComponent{
+																Type: linebot.FlexComponentTypeText,
+																Text: "'" + randomElements[0].Kami + "'",
+																Weight: "regular",
+																Size: "md",
+																Align: "center",
+														},
+														&linebot.TextComponent{
+															Type: "text",
+															Text: "下の句はどれでしょう？",
+															Weight: "regular",
+															Size: "md",
+															Align: "center",
+															Margin: "md",
+														},
+												},
+										},
+										Footer: &linebot.BoxComponent{
+											Type:   linebot.FlexComponentTypeBox,
+											Layout: linebot.FlexBoxLayoutTypeVertical,
+											Spacing: linebot.FlexComponentSpacingTypeSm,
+											Contents: []linebot.FlexComponent{
+													&linebot.ButtonComponent{
+															Type: linebot.FlexComponentTypeButton,
+															Style: linebot.FlexButtonStyleTypeLink,
+															// ここ修正する
+															Action: linebot.NewMessageAction(choices[0], "正解"),
+															},
+													&linebot.ButtonComponent{
+															Type: linebot.FlexComponentTypeButton,
+															Style: linebot.FlexButtonStyleTypeLink,
+															Action: linebot.NewMessageAction(choices[1], "はずれ"),
+															},
+													&linebot.ButtonComponent{
+															Type: linebot.FlexComponentTypeButton,
+															Style: linebot.FlexButtonStyleTypeLink,
+															Action: linebot.NewMessageAction(choices[2], "はずれ"),
+															},
+													&linebot.ButtonComponent{
+															Type: linebot.FlexComponentTypeButton,
+															Style: linebot.FlexButtonStyleTypeLink,
+															Action: linebot.NewMessageAction(choices[3], "はずれ"),
+															},
+													},
+											},
+									},
+						)
+						_, err = bot.ReplyMessage(event.ReplyToken, resp).Do()
+							if err != nil {
+											log.Print(err)
+							}
 			}
 		}
 	}
+}
 }
